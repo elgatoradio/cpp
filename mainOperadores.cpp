2520465227441f658f37728533bf35e2b281778b
elgatoradio <elgatoradi@gmail.com>
2022-07-05 12:44:07 -0500

    update  operadores.cpp

---
+++ mainOperadores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int edad = 0;
+    cout << "¿Cual es tu edad?" << endl;
+    cin >> edad;
+    cout << "tu edad es: " << edad << endl;
+}
