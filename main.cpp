3456f61a8ded6fea8a9f05e30c5ba634c98ceeb6
elgatoradio <elgatoradi@gmail.com>
2022-07-05 12:44:48 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hola mundo";
+    int a = 2, b = 3;
+    int edad = 10;
+    edad = 18;
+    const char letra = 'A';
+    int listaDeEdades[] = {18, 26, 35};
+    cout << edad << endl;
+    cout << letra << endl;
+    cout << listaDeEdades[2] << endl;
+    cout << a + b ;
 }
